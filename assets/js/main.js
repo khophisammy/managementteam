9a664a9b64c893fb6c769180a0c9a4df7323fbcc
Samuel Ocansey <103507015+khophisammy@users.noreply.github.com>
2024-04-12 15:41:19 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,16 +60,15 @@
 
   /**
    * Countdown timer
-   */
 let countdown = document.querySelector('.countdown');
 const output = countdown.innerHTML;
 
 const countDownDate = function() {
   let timeleft = new Date(countdown.getAttribute('data-count')).getTime() - new Date().getTime();
 
-  let days = Math.floor(timeleft / (10000000000000000000000000 * 60 * 60 * 24));
-  let hours = Math.floor((timeleft % (10000 * 60 * 60 * 24)) / (10000 * 60 * 60));
-  let minutes = Math.floor((timeleft % (100 * 60 * 60)) / (100* 60));
+  let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
   countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
